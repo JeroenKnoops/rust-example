030b72500f08774b685c59c3e5ddd64afce432f1
Jeroen Knoops <jeroen.knoops@gmail.com>
2018-08-12 16:58:49 +0200

    Adds functions 'add' and 'substract'

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn substract(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+}
